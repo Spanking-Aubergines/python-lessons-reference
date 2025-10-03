47833fb7b38c845097ceaa88a722bae9cbe19f34
Spanking-Aubergines <ricardoscosta1995@gmail.com>
2025-10-03 18:16:20 +0100

    ALIAS

---
+++ 04-PR_madlibs_game.py
@@ -0,0 +1,12 @@
+# wordgame where you create a story by filling the blanks
+
+adjective1 = str(input("type an adjective: "))
+adjective2 = str(input("type an adjective: "))
+adjective3 = str(input("type an adjective: "))
+noun1 = str(input("type an noun: "))
+verb1 = str(input("type an verb: "))
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}.")
+print(f"{noun1} was {adjective2} and {verb1}.")
+print(f"I was {adjective3} !")
