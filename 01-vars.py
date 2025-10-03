e69657b2e24a79f09c91642df509e1de9ced4dff
Spanking-Aubergines <ricardoscosta1995@gmail.com>
2025-10-03 12:07:09 +0100

    ALIAS

---
+++ 01-vars.py
@@ -0,0 +1,21 @@
+
+# strings
+first_name = "asd"
+last_name = "asdasd"
+
+# int
+age = 25
+quantity = 12
+
+# float
+price = 10.99
+
+# bool
+is_male = True
+is_lefthanded = False
+
+print(f"hello {first_name}")
+print(f"my last name is {last_name}")
+print(f"i am {age} years old")
+print(f"i have {quantity} cars")
+print(f" i charge {price}€ per hour")
---
+++ 02-typecasting.py
@@ -0,0 +1,11 @@
+# converting a variable type from one data type to another
+
+name = "Ruibardo Tavares"
+age = 24
+avg = 12.4
+passed = True
+
+print(type(name))
+print(type(age))
+print(type(avg))
+print(type(passed))
