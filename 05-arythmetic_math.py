07a51591d70fa391831d6d01a6b9ec64aa6ab2b1
Spanking-Aubergines <ricardoscosta1995@gmail.com>
2025-10-04 02:03:38 +0100

    ALIAS

---
+++ 05-arythmetic_math.py
@@ -0,0 +1,70 @@
+import math
+
+friends = 3
+
+# sum + augmented
+
+# friends = friends + 1
+# friends += 1
+
+# subtract + augmented
+
+# friends = friends - 1
+# friends -= 1
+
+# multiplication + augmented
+# friends = friends * 3
+# friends *=3
+
+# division + augmented
+# friends = friends / 3
+# friends /= 2
+
+# power + augmented
+# friends = friends ** 2
+# friends **=2
+
+# modulus + augmented - resto de divisão
+# rest = friends %3
+
+# print(friends)
+
+# //////////////functions/////////////////
+
+x = 3.323
+y = 4
+z = 6
+
+# round to nearest int
+print(round(x)) 
+
+# distance from 0
+print(abs(y))
+
+# x to the power of y
+print(pow(1,2))
+
+# max of number list
+print(max(1,2,3))
+
+# min of numbers
+print(min(1,2,3))
+
+# ////////////////math module//////////////
+# needs "import math"
+
+#pi
+print(math.pi)
+
+# e
+print(math.e)
+
+# square root
+print(math.sqrt(y))
+
+# ceiling func -  always round float up
+print(math.ceil(x))
+
+# floor - rounds float down
+print(math.floor(x))
+
---
+++ 05.1-Ex_circle_area.py
@@ -0,0 +1,9 @@
+import math
+
+# formula 2xpixr
+
+radius = float(input("Enter the radius of the circle: "))
+area = (2*math.pi*radius)
+print(f"A circle with a radius of {radius}, has {area}cm2")
+
+
