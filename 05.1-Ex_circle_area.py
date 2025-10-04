c9a966210723cf97a647e8e79959314618ff3986
Spanking-Aubergines <ricardoscosta1995@gmail.com>
2025-10-04 02:06:44 +0100

    ALIAS

--- 05.1-Ex_circle_area.py
+++ 05.1-Ex_circle_area.py
@@ -3,7 +3,7 @@ import math
 # formula 2xpixr
 
 radius = float(input("Enter the radius of the circle: "))
-area = (2*math.pi*radius)
-print(f"A circle with a radius of {radius}, has {area}cm2")
+area = round((2*math.pi*radius),2)
+print(f"A circle with a radius of {radius}, has {area}cm of circumference")
 
 
