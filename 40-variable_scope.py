3c99af4b6bf8807bce59532cbbb874823b987863
Spanking-Aubergines <ricardoscosta1995@gmail.com>
2025-10-20 00:09:00 +0100

    ALIAS

--- 40-variable_scope.py
+++ 40-variable_scope.py
@@ -5,4 +5,38 @@
 def func1():
     a = 1
     print(a)
+
+func1()
 # var inside funcs are local
+
+
+def func2():
+    x = 1
+    def func3():
+    func3()
+
+func2()
+
+# var enclosed in func 2 but can be called by func3
+
+def func4():
+    print(x)
+
+def func5():
+    print(x)
+
+x = 3
+
+func4()
+func5()
+
+# var global has been defined outside of the func
+
+from math import e
+
+def func6()
+    print(e)
+
+func6()
+
+# var built int 
---
+++ 41-ifmainequalsname.py
@@ -0,0 +1,10 @@
+# if __name__ == __main__: (this script can be imported or run standalone)
+# Function and classes in this module can be reused without the main block of code executing
+# Good practice ( code is modular
+                    # helps readability,
+                    # leaves no global variables,
+                    # avoid unintened execution
+
+                    # ex: library = impor library for functionality
+                        # when running library directly, display a help page
+
